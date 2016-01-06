ce55ed77eecbfdb6b5a024d16352b5cd5c58deb2
ShMcK <shawn.j.mckay@gmail.com>
2016-01-05 18:07:32 -0800

    fix

--- context.js
+++ context.js
@@ -5,7 +5,7 @@ var vm = require('vm');
  * load context
  * runs a test within the context of another file
  */
-module.exports = function loadContext(pathToContext: string): void {
+module.exports = function loadContext(pathToContext) {
   var context = fs.readFileSync(pathToContext);
   vm.runInThisContext(context);
-}
+};
