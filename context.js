50bed03654bb608a162efd60ab610242dff68fa5
ShMcK <shawn.j.mckay@gmail.com>
2016-01-05 19:35:18 -0800

    setup for compiling fileTypes

--- context.js
+++ context.js
@@ -7,6 +7,25 @@ var vm = require('vm');
  * pathToContext: a relative path to the file
  */
 module.exports = function loadContext(pathToContext) {
-  var context = fs.readFileSync(__dirname + pathToContext);
-  vm.runInThisContext(context);
+  var fileType = getExtension(pathToContext);
+
+  console.log('ext', fileType);
+
+  switch (fileType) {
+    case '.json':
+    case '.js':
+      var context = fs.readFileSync(pathToContext);
+      vm.runInThisContext(context);
+      return;
+
+    default:
+      var error = 'File type ' + fileType + ' not supported. Cannot load unit test from context.';
+      console.log(error);
+      throw (error);
+  }
 };
+
+function getExtension(string) {
+  var extensionRegex = /\.[0-9a-z]+$/i;
+  return extensionRegex.exec(string)[0];
+}
