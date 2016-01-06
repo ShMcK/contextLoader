6b37417ed776a40147350f2a157b74ca2ef0ec34
ShMcK <shawn.j.mckay@gmail.com>
2016-01-05 18:41:17 -0800

    use relative path

--- context.js
+++ context.js
@@ -4,8 +4,9 @@ var vm = require('vm');
 /**
  * load context
  * runs a test within the context of another file
+ * pathToContext: a relative path to the file
  */
 module.exports = function loadContext(pathToContext) {
-  var context = fs.readFileSync(pathToContext);
+  var context = fs.readFileSync(__dirname + pathToContext);
   vm.runInThisContext(context);
 };
